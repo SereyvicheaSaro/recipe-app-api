481e6e65c0638b8b450c4d7c7e2c22c864f5f2ea
Vichea <104241903+CheaTXT@users.noreply.github.com>
2024-04-19 10:04:00 +0700

    Django unit test

---
+++ app/app/tests.py
@@ -0,0 +1,17 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app  import cal
+class CalTests(SimpleTestCase):
+    """Test teh cal module. """
+    def test_add_numbers(self):
+        """Test adding numbers togerther."""
+        res = cal.add(5, 6)
+        self.assertEqual(res, 11)
+    
+    def test_substract_number(self):
+        """Test substract numbers."""
+        res = cal.substract(10, 9)
+        self.assertEqual(res, 1)
