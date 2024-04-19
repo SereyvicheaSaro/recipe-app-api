0d4459cf060ffcdd4f22c557fe57e4ef9f632d01
Vichea <104241903+CheaTXT@users.noreply.github.com>
2024-04-19 15:03:24 +0700

    install Psycopg2 to project via Docker file

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,7 +6,7 @@ from django.test import SimpleTestCase
 from app  import cal
 class CalTests(SimpleTestCase):
     """Test teh cal module. """
-    def test_add_numbers(self):
+    def test_add_numbers(self):     # make sure that the function should start with ** test_ **
         """Test adding numbers togerther."""
         res = cal.add(5, 6)
         self.assertEqual(res, 11)
