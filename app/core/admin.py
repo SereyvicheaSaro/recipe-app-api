d7455ab8e10e765c7b475de371863627f3212aed
Vichea <104241903+CheaTXT@users.noreply.github.com>
2024-04-27 23:00:35 +0700

    fixed error

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,8 +3,8 @@ Django admin customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from core import models
 from django.utils.translation import gettext_lazy as _
+from core import models
 
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
