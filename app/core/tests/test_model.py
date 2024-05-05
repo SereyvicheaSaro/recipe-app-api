4b5d3ceb97a9e5b90b3cebee5ec023e9b5cd7e78
Vichea <104241903+CheaTXT@users.noreply.github.com>
2024-05-05 08:28:12 +0700

    finish recipe API

--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -1,8 +1,11 @@
 """
 Test for model
 """
+
+from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
 
 class ModelTests(TestCase):
     """Test models."""
@@ -42,3 +45,19 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    # test recipe model
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user = user,
+            title = 'Sample',
+            time_minutes = 5,
+            price = Decimal('5.50'),
+            description = 'Sample description.',
+        )
+        self.assertEqual(str(recipe), recipe.title)
